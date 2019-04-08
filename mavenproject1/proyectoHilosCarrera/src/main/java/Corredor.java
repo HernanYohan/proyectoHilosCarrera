370716934777c6a437a7ca54c2be02e1f437e530
Hernan Hernandez <hernanyohh@hotmail.com>
2019-04-08 17:34:38 -0500

    Logica clases Corredor y Grupo

--- mavenproject1/proyectoHilosCarrera/src/main/java/Corredor.java
+++ mavenproject1/proyectoHilosCarrera/src/main/java/Corredor.java
@@ -1,3 +1,7 @@
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,12 +12,72 @@
  *
  * @author Hernan
  */
-public class Corredor {
+public class Corredor  extends Thread{
     
-    private int nAtleta;
+    private int nCorredor;
     private int salida;
-    private int entrada;
+    private int llegada;
     public Grupo grupo;
     public Utilitarios ut;
+
+    public Corredor(int nCorredor, int salida, int llegada, Grupo grupo, Utilitarios ut) {
+        this.nCorredor = nCorredor;
+        this.salida = salida;
+        this.llegada = llegada;
+        this.grupo = grupo;
+        this.ut = ut;
+    }
     
+   public void pausaInicioHilos (){
+   do{
+    if (grupo.getPasos()<salida){
+        synchronized(grupo){
+            try{
+                grupo.wait();
+            }catch (InterruptedException ex){
+                Logger.getLogger(Corredor.class.getName()).log(Level.SEVERE,null,ex);
+            }
+            
+        }
+   }else{
+        continue;
+    }
+    
+   }while(grupo.getPasos()<salida);
+   
+   while(grupo.getPasos()<grupo.getMeta() && grupo.getPasos() < llegada ){
+       try{
+           int numeroAleatorio = ut.numeroRandom();
+           grupo.setPasos(+numeroAleatorio);
+           Thread.sleep(500);
+           
+            if(grupo.getPasos()>= grupo.getMeta()){
+                
+                System.out.println("\n EQUIPO GANADOR: "+grupo.getNumeroGrupo()+" ,Corredor: "+ nCorredor);
+                        
+                    System.exit(0);
+                
+            }else{
+                    synchronized(grupo){
+                        if(grupo.getPasos()>=llegada){
+                            grupo.notifyAll();
+                        }
+                    }
+                    
+                }
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Corredor.class.getName()).log(Level.SEVERE, null, ex);   
+            }
+       
+       
+   }
+   
+   
+   
+   } 
+  
+   @Override
+    public void run() {  
+        pausaInicioHilos();
+   } 
 }
--- mavenproject1/proyectoHilosCarrera/src/main/java/Grupo.java
+++ mavenproject1/proyectoHilosCarrera/src/main/java/Grupo.java
@@ -9,5 +9,146 @@
  * @author Hernan
  */
 public class Grupo {
+    /**
+     * Variable que indica la meta 
+     */
+    
+    private int meta;
+    
+    /**
+     * variable que indica el numero de pasos que ha dado 
+     */
+    private int pasos;
+    /**
+     * Variable que indica el grupo al que pertenece 
+     */
+    private int numeroGrupo;
+    /**
+     * Variable que dice la figura de cada corredor 
+     */
+    private String figura;
+
+    
+    /**
+     * Constructor que inicializa todas las variable 
+     * @param meta
+     * @param pasos
+     * @param numeroGrupo
+     * @param figura 
+     */
+    public Grupo(int meta, int pasos, int numeroGrupo, String figura) {
+        this.meta = meta;
+        this.pasos = pasos;
+        this.numeroGrupo = numeroGrupo;
+        this.figura = figura;
+    }
+    
+    public void imprimirRecorrido(){
+        String recorrido=("\n"); 
+        if(pasos<33){
+            for(int i=0;i<pasos;i++){
+                 recorrido = recorrido+"_";
+            }
+            recorrido+=figura;
+                for(int i=pasos;i<32;i++){
+                    recorrido = recorrido+"_";
+                }
+                recorrido+=figura;
+                 
+                    for(int i=33;i<66;i++){
+                    recorrido = recorrido+"_";
+                    }
+                    recorrido+=figura;
+                    
+                        for(int i=66;i<=100;i++){
+                            recorrido = recorrido+"_";
+                        }
+                        
+                        System.out.println("Equipo numero: "+numeroGrupo+"   "+ recorrido);
+        }
+        
+        if (pasos >= 33 && pasos < 66) {
+            
+            for (int i = 0; i < 33; i++) {
+                recorrido = recorrido + ".";
+            }
+            recorrido += figura;
+            for (int i = 33; i < pasos; i++) {
+                recorrido = recorrido + ".";
+            }
+            recorrido += figura;
+            for (int i = pasos; i < 65; i++) {
+                recorrido = recorrido + ".";
+            }
+            recorrido += figura;
+            for (int i = 66; i <= 100; i++) {
+                recorrido = recorrido + ".";
+            }
+
+            System.out.print("Equipo numero: "+numeroGrupo+"   "+ recorrido);
+
+        }
+        if (pasos >= 66) {
+           
+            for (int i = 0; i < 33; i++) {
+                recorrido = recorrido + ".";
+            }
+            recorrido += figura;
+            for (int i = 33; i < 66; i++) {
+                recorrido = recorrido + ".";
+            }
+            recorrido += figura;
+            for (int i = 66; i <= pasos; i++) {
+                recorrido = recorrido + ".";
+            }
+            recorrido += figura;
+            for (int i = pasos; i < 99; i++) {
+                recorrido = recorrido + ".";
+            }
+
+            System.out.print( recorrido);
+
+        }
+
+        System.out.print("Equipo numero: "+numeroGrupo+"   "+ recorrido);
+
+    }
+    
+        public int retornarPasos(){
+            return pasos;
+         }
+
+    public int getMeta() {
+        return meta;
+    }
+
+    public void setMeta(int meta) {
+        this.meta = meta;
+    }
+
+    public int getPasos() {
+        return pasos;
+    }
+
+    public void setPasos(int pasos) {
+        this.pasos = pasos;
+    }
+
+    public int getNumeroGrupo() {
+        return numeroGrupo;
+    }
+
+    public void setNumeroGrupo(int numeroGrupo) {
+        this.numeroGrupo = numeroGrupo;
+    }
+
+    public String getFigura() {
+        return figura;
+    }
+
+    public void setFigura(String figura) {
+        this.figura = figura;
+    }
+    
     
 }
